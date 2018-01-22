f1b08d5ebee71be960ab6aa93ac4fb676288bb3b
Ève-Lyne Marion <elmarion@volcan.design>
2018-01-22 10:41:35 -0500

    component volcan-hi

---
+++ src/components/Volcan-Hi.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="hi">
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Volcan-H1',
+  data () {
+    return {
+      msg: 'Volcan vous dit bonjour!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #ef7d0b;
+}
+
+</style>
